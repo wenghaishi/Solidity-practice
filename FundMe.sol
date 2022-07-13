b468057e94a8c13bc20280820f91c0ea81bf690e
sam <wenghaishi1@gmail.com>
2022-07-13 12:36:43 +0800

    modified FundMe

--- FundMe.sol
+++ FundMe.sol
@@ -2,9 +2,23 @@
 
 pragma solidity ^0.8.0;
 
+uint256 public minumumUsd = 50 * 10 ** 18;
+
+import "@chainlink/contracts/src/v0.6/interfaces/aggregatorv3interface.sol";
+
 contract FundMe {
     function fund() public payable {
-        require(msg.value > 1e18);
-
+        require(msg.value >= minimumUsd, "didn't send enough");
     }
+
+    function getPrice() public {
+        //ABI
+        //Address 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+    }
+
+    function getVersion() public view returns(int256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e).version();
+        return priceFeed.version();
+    }
+        
 }
