f1cafdfb0056a8a0f397756509e3d941524b818f
sam <wenghaishi1@gmail.com>
2022-09-02 22:35:25 +0800

    created vendingMachine contract

--- ToDos.sol
+++ ToDos.sol
@@ -3,10 +3,17 @@
 pragma solidity ^0.8.16;
 
 contract ToDos {
-    struct ToDo {
-        string task;
+    uint256 taskCount = 0;
+    struct Task {
+        uint id;
+        string content;
         bool completion;
     }
 
-    ToDo[] public todos;
-}
+    Task[] public task;
+
+    mapping(uint => Task) public tasks;
+
+    function newTask(string memory _task) public {
+        taskCount++;
+        tasks[taskCount] = Tas
---
+++ VendingMachine.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+contract VendingMachine {
+    address public owner;
+    mapping (address => uint) public donutBalances;
+
+    constructor() {
+        owner = msg.sender;
+        donutBalances[address(this)] = 100;
+    }
+
+    modifier _ownerOnly() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function getVendingMachineBalance() public view returns(uint256) {
+        return donutBalances[address(this)];
+    }
+
+    function restock(uint256 _amount) private _ownerOnly {
+        donutBalances[address(this)] += _amount;
+    }
+
+    function purchase(uint _amount) public payable {
+        require(msg.value == _amount * 1 ether, "You must pay 2 ether per donut");
+        require(donutBalances[address(this)] >= _amount, "out of stock");
+        donutBalances[address(this)] -= _amount;
+        donutBalances[msg.sender] += _amount;
+    }
+    
+}
